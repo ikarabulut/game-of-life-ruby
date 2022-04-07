ec02373d4d651fe5fb9671b4dfde70785f3002da
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-07 15:00:08 -0500

    adds test stubs and first test

---
+++ test/ui_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require_relative '../lib/ui.rb'
+
+class UI_Test < Minitest::Test
+
+  class StubInputGetter
+    def get_alive_symbol
+     "o"
+    end
+  
+    def get_dead_symbol
+      "x"
+    end
+  
+    def set_grid_size
+      25
+    end
+  
+    def set_iterations_number
+      40
+    end
+  end
+
+  def test_user_can_set_alive_cell_symbol
+    ui = UI.new(StubInputGetter.new)
+    ui.set_alive_symbol
+    assert_equal(ui.alive_symbol, "o")
+  end
+
+
+
+end
