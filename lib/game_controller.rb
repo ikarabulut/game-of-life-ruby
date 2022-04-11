aa8ac56e72f3cc848eb63cc18064a7ab561f9d1c
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-11 11:41:57 -0500

    adds initialize and start methods

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,7 +1,19 @@
-
+require_relative 'game_settings'
+require_relative 'world'
+require_relative 'board'
 
 class GameController
 
+  def initialize
+    @game_settings = GameSettings.new
+    @board = Board.new(@game_settings)
+    @world = World.new(@board)
+  end
 
+  def start
+    @game_settings.settings_prompt
+    @world.generate_random_cells
+    @world.begin_evolutions
+  end
 
 end
