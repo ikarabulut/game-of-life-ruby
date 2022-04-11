b5f4533ffdf5242c472ab38e0ba073eedd7911e8
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-11 15:18:52 -0500

    passes @cells into Display methods

--- lib/world.rb
+++ lib/world.rb
@@ -31,32 +31,32 @@ class World
   def neighbors_of(x, y)
     neighbors = []
     neighbors.push(@cells[x][y - 1]) # Left
-    if y == @columns - 1
+    if y == @settings.columns - 1
       neighbors.push(@cells[x][0]) # Wrap from right to left
     else
       neighbors.push(@cells[x][y + 1]) # Right
     end
-    if x == @rows - 1
+    if x == @settings.rows - 1
       neighbors.push(@cells[0][y]) # Wrap from bottom to top
     else
       neighbors.push(@cells[x + 1][y]) # Bottom
     end
-    if y == @columns - 1 && x == @rows - 1
+    if y == @settings.columns - 1 && x == @settings.rows - 1
       neighbors.push(@cells[0][0]) # Wrap from bottom right to top right if cell == bottom right corner
-    elsif y == @columns - 1
+    elsif y == @settings.columns - 1
       neighbors.push(@cells[x + 1][0]) # Wrap from right to left
-    elsif x == @rows - 1
+    elsif x == @settings.rows - 1
       neighbors.push(@cells[0][y + 1]) # Wrap from bottom to top
     else  
       neighbors.push(@cells[x + 1][y + 1]) # Bottom Right
     end
-    if x == @rows - 1
+    if x == @settings.rows - 1
       neighbors.push(@cells[0][y - 1]) # Wrap from bottom to top
     else
       neighbors.push(@cells[x + 1][y - 1]) # Bottom left
     end
     neighbors.push(@cells[x - 1][y]) # Top
-    if y == @columns - 1
+    if y == @settings.columns - 1
       neighbors.push(@cells[x - 1][0]) # Wrap from right to left
     else
       neighbors.push(@cells[x - 1][y + 1]) # Top Right
@@ -79,8 +79,8 @@ class World
     new_world = @display.generate_board
     old_world.each do |cell|
       new_cell = Cell.new(cell.x, cell.y)
-      cell = old_world[cell.x][cell.y]
-      if cell.alive_next_generation?
+      # cell = old_world[cell.x][cell.y]
+      if alive_next_generation?(cell.x, cell.y)
         new_cell.revive
         new_world[cell.x][cell.y] = new_cell
       end
@@ -96,7 +96,7 @@ class World
         system "clear"
         puts "Generation #{generation_number += 1}"
         tick
-        @display.print_board
+        @display.print_board(@cells)
         sleep(1)
       end
     else
@@ -104,7 +104,7 @@ class World
         system "clear"
         puts "Generation #{generation_number += 1}"
         tick
-        @display.print_board
+        @display.print_board(@cells)
         sleep(1)
       end
     end
