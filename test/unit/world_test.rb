86c85e81b2996c0bd58d5f1311fa11911c7911d5
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-12 15:10:57 -0500

    adds remaining rules tests and fixes GOL-3-BUG in World#alive_next_generation?

--- test/unit/world_test.rb
+++ test/unit/world_test.rb
@@ -38,7 +38,53 @@ class WorldTest < MiniTest::Test
     world.cells[2][5].revive
     world.cells[2][6].revive
     world.cells[3][5].revive
-    assert(true, world.alive_next_generation?(2, 5))
+    assert(world.alive_next_generation?(2, 5))
+  end
+
+  def test_that_an_alive_cell_with_3_alive_neighbors_stays_alive
+    world = World.new(FakeDisplay.new)
+    world.generate_dead_cells
+    world.cells[1][5].revive
+    world.cells[2][4].revive
+    world.cells[2][5].revive
+    world.cells[2][6].revive
+    world.cells[3][5].revive
+    assert(world.alive_next_generation?(1, 5), "An alive cell with 3 alive neighbors did survive an evolution")
+  end
+
+  def test_that_an_alive_cell_with_more_than_3_alive_neighbors_does_not_survive
+    world = World.new(FakeDisplay.new)
+    world.generate_dead_cells
+    world.cells[1][5].revive
+    world.cells[2][4].revive
+    world.cells[2][5].revive
+    world.cells[2][6].revive
+    world.cells[3][5].revive
+    refute(world.alive_next_generation?(2, 5), "An alive cell with more than 3 alive neighbors survived when it should have died")
+  end
+
+  def test_that_an_alive_cell_with_less_than_2_alive_neighbors_does_not_survive
+    world = World.new(FakeDisplay.new)
+    world.generate_dead_cells
+    world.cells[2][5].revive
+    refute(world.alive_next_generation?(2, 5), "An alive cell with less than 2 alive neighbors survived when it should have died")
+  end
+
+  def test_that_a_dead_cell_with_exactly_3_alive_neighbors_will_come_to_life
+    world = World.new(FakeDisplay.new)
+    world.generate_dead_cells
+    world.cells[2][5].revive
+    world.cells[2][6].revive
+    world.cells[3][5].revive
+    assert(world.alive_next_generation?(3, 6), "A dead cell with exactly 3 alive neighbors stayed dead when it should have revived")
+  end
+
+  def test_that_a_dead_cell_without_exactly_3_alive_neighbors_stays_dead
+    world = World.new(FakeDisplay.new)
+    world.generate_dead_cells
+    world.cells[2][5].revive
+    world.cells[2][6].revive
+    refute(world.alive_next_generation?(3, 6), "A dead cell without exactly 3 alive neighbors revived when it should have stayed dead")
   end
 
 
