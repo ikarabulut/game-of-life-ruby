55d5a794d1b5079960bb16206d1f50e557eeffd1
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-14 16:06:39 -0500

    adds test for #settings_prompt to ensure set_game_settings is called if conditional passes

--- test/unit/game_settings_test.rb
+++ test/unit/game_settings_test.rb
@@ -16,8 +16,47 @@ class GameSettingsTest < MiniTest::Test
     game_settings.set_dead_symbol
     assert_equal("x", game_settings.dead_symbol)
   end
+  
+  def test_that_the_number_of_rows_can_be_updated
+    fake_input_getter = FakeInputGetter.new
+    game_settings = GameSettings.new(fake_input_getter)
+    game_settings.set_number_of_rows
+    assert_equal(10, game_settings.rows)
+  end
 
+  def test_that_the_number_of_columns_can_be_updated
+    fake_input_getter = FakeInputGetter.new
+    game_settings = GameSettings.new(fake_input_getter)
+    game_settings.set_number_of_columns
+    assert_equal(25, game_settings.columns)
+  end
 
+  def test_that_the_number_of_evolutions_can_be_updated
+    fake_input_getter = FakeInputGetter.new
+    game_settings = GameSettings.new(fake_input_getter)
+    game_settings.set_number_of_evolutions
+    assert_equal(40, game_settings.evolutions)
+  end
+
+  def test_that_defaults_can_be_set_to_false
+    fake_input_getter = FakeInputGetter.new
+    game_settings = GameSettings.new(fake_input_getter)
+    game_settings.set_defaults_prompt
+    assert_equal(false, game_settings.defaults)
+  end
+
+  def test_that_set_game_settings_is_called_if_defaults_are_false
+    fake_input_getter = FakeInputGetter.new
+    game_settings = GameSettings.new(fake_input_getter)
+
+    settings_prompt_mock = MiniTest::Mock.new
+    settings_prompt_mock.expect :call, true
+
+    game_settings.stub :set_game_settings, settings_prompt_mock do
+      game_settings.settings_prompt
+    end
+    assert(settings_prompt_mock.verify)
+  end
 
 
 
@@ -42,5 +81,9 @@ class FakeInputGetter
 
   def get_number_of_evolutions
     40
+  end
+
+  def get_defaults_prompt_response
+    false
   end
 end
