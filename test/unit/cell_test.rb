f2e8600d32f88674520f3fafa0b901c2f64949c2
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-14 11:56:59 -0500

    adds full test coverage for Cell class

--- test/unit/cell_test.rb
+++ test/unit/cell_test.rb
@@ -4,7 +4,22 @@ require_relative "../../lib/cell.rb"
 class CellTest < Minitest::Test
 
 
-  
+  def test_that_a_cell_that_is_alive_returns_true
+    cell = Cell.new(1, 1, true)
+    assert(cell.alive?)
+  end
+
+  def test_that_a_dead_cell_can_be_revived
+    cell = Cell.new(1, 1, false)
+    cell.revive
+    assert(cell.alive?)
+  end
+
+  def test_that_an_alive_cell_can_die
+    cell = Cell.new(1, 1, true)
+    cell.die
+    refute(cell.alive?)
+  end
 
 
 end
