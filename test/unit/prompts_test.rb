eb2f50c5d31755f5bb701b53c2340623b0cd7628
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-14 17:06:48 -0500

    passes test

--- test/unit/prompts_test.rb
+++ test/unit/prompts_test.rb
@@ -4,17 +4,17 @@ require_relative "../../lib/prompts.rb"
 class PromptsTest < MiniTest::Test
 
   def test_that_messages_are_printed_to_the_terminal
-    # fake_game_settings = FakeGameSettings.new
-    # prompts = Prompts.new(fake_game_settings)
+    fake_game_settings = FakeGameSettings.new.game_settings
+    prompts = Prompts.new(fake_game_settings)
 
-    # expected = "Welcome to Conways Game of Life!" + "\n" +     "The defaults for this game are: \n" +
-    # "Alive cell: 1 \n" +
-    # "Dead cell: 0 \n" +
-    # "Grid Size: 10 X 10 \n" +
-    # "Evolutions: infinity" + "\n" +
-    # "Would you like to play with the defaults? (y/n)"
-
-    # assert_output(expected) { prompts.print_messages }
+    expected = "Welcome to Conways Game of Life!" + "\n" + "The defaults for this game are: \n" +
+    "Alive cell: 1 \n" +
+    "Dead cell: 0 \n" +
+    "Grid Size: 10 X 10 \n" +
+    "Evolutions: infinity" + "\n" +
+    "Would you like to play with the defaults? (y/n)
+"
+    assert_output(expected) { prompts.print_messages }
   end
 
 end
