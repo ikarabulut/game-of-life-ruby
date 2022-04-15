9503510ed11cd1fe4e702e10b710d6f1a1fa1118
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-15 12:32:14 -0500

    updates GenerateWorld integration test to pass

--- test/integration/generate_world_test.rb
+++ test/integration/generate_world_test.rb
@@ -4,10 +4,13 @@ require_relative '../../lib/world.rb'
 class GenerateWorldTest < MiniTest::Test
 
   def test_generating_a_random_board
-    world = World.new
+    input_getter = InputGetter.new
+    game_settings = GameSettings.new(input_getter)
+    display = Display.new(game_settings)
+    world = World.new(display, SystemInteraction.new)
+
     world.generate_random_cells
-    board = world.board
-    board.flatten.each do |cell|
+    world.cells.flatten.each do |cell|
       refute_nil(cell)
     end
   end
