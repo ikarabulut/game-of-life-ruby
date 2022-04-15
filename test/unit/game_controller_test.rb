ffd96e53daac089047477590aec5cfc7482ee6b5
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-15 10:25:47 -0500

    updates #get_display test, to take in variable vs. a method call

--- test/unit/game_controller_test.rb
+++ test/unit/game_controller_test.rb
@@ -10,7 +10,8 @@ class GameControllerTest < Minitest::Test
 
   def test_that_Display_is_instantiated_when_get_display_is_called
     game_controller = GameController.new
-    assert_instance_of(Display, game_controller.get_display(game_controller.get_settings))
+    game_settings = GameSettings.new(game_controller.input_getter)
+    assert_instance_of(Display, game_controller.get_display(game_settings))
   end
 
   def test_that_World_is_instantiated_when_create_world_is_called
