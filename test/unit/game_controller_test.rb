7e74ad83610618cad01701d1e7aaa451236ba749
Ismail Karabulut <i.karabulut93@gmail.com>
2022-04-14 21:38:29 -0500

    adds get_display test

--- test/unit/game_controller_test.rb
+++ test/unit/game_controller_test.rb
@@ -3,12 +3,15 @@ require_relative '../../lib/game_controller.rb'
 
 class GameControllerTest < Minitest::Test
 
-  def test_that_GameSettings_is_instantiated
+  def test_that_GameSettings_is_instantiated_when_get_settings_is_called
     game_controller = GameController.new
     assert_instance_of(GameSettings, game_controller.get_settings)
   end
 
-
+  def test_that_Display_is_instantiated_when_get_display_is_called
+    game_controller = GameController.new
+    assert_instance_of(Display, game_controller.get_display(game_controller.get_settings))
+  end
 
 
 end
